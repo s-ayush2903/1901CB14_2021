f49f99e6460d2e00f0d9e28db3ee436398f98573
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2021-10-22 15:28:10 +0530

    Add code for tutorial#6 submission

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,15 +1,82 @@
+import os
+import re
+import shutil
+
+pwd = os.getcwd()
+wrongRoot = os.path.join(pwd, "wrong_srt")
+correctRoot = os.path.join(pwd, "corrected_srt")
+
+"""
+Renames the files of the specified directory semantically via truncating
+redundant information using Regex
+Accepts
+@param seriesName: Name of Web Series which needs to be renamed
+@param cleanerRegex: The Regex used to truncate / strip the unnecessary
+information provided unsolicitely in the raw file name
+@param sznAndEpRegex: The Regex used to extract out the string which contains
+the information regarding the season number and episode number of the concerned
+series in process
+@param uniqueSeparator: The unique separator in the string containing fused Web
+Series Season plus Episode information, separated via the separator being
+discussed here
+"""
+def generic_rename(seriesName, cleanerRegex, sznAndEpRegex, uniqueSeparator):
+    print(f"Renaming '{seriesName}'...")
+    seriesWrongRoot = os.path.join(wrongRoot, seriesName)
+    seriesCorrectRoot = os.path.join(correctRoot, seriesName)
+
+    if os.path.exists(seriesCorrectRoot):
+        print(
+            f"[WARNING!] destination {seriesCorrectRoot} already exists, overwriting contents!"
+        )
+        shutil.rmtree(seriesCorrectRoot)
+    os.mkdir(seriesCorrectRoot)
+
+    for filename in os.listdir(seriesWrongRoot):
+        rawInfo = re.split(cleanerRegex, filename)
+        mergedInfo = re.split(sznAndEpRegex, rawInfo[0])
+        sznAndEpList = list(map(int, re.split(uniqueSeparator, mergedInfo[1])))
+        desiredFileName = (
+            mergedInfo[0]
+            + (" -" if webseries_num != 1 else "")
+            + " Season "
+            + str(sznAndEpList[0]).zfill(season_padding)
+            + " Episode "
+            + str(sznAndEpList[1]).zfill(episode_padding)
+            + (" -" if webseries_num != 1 else "")
+            + ((" " + mergedInfo[2]) if (len(mergedInfo) > 2) else "")
+            + rawInfo[1]
+        )
+        # print(desiredFile)
+        absIncorrectDest = os.path.join(seriesWrongRoot, filename)
+        absCorrectDest = os.path.join(seriesCorrectRoot, desiredFileName)
+        shutil.copy(absIncorrectDest, absCorrectDest)
+        # print(f"Copied \n{absIncorrectDest} to \n{absCorrectDest}")
+    return
+
 def regex_renamer():
+    # Taking input from the user
+    print("1. Breaking Bad")
+    print("2. Game of Thrones")
+    print("3. Lucifer")
 
-	# Taking input from the user
-
-	print("1. Breaking Bad")
-	print("2. Game of Thrones")
-	print("3. Lucifer")
-
-	webseries_num = int(input("Enter the number of the web series that you wish to rename. 1/2/3: "))
-	season_padding = int(input("Enter the Season Number Padding: "))
-	episode_padding = int(input("Enter the Episode Number Padding: "))
-
+    global season_padding, episode_padding, webseries_num
+    webseries_num = int(
+        input("Enter the number of the web series that you wish to rename. 1/2/3: ")
+    )
+    if webseries_num > 0 and webseries_num < 4:
+        season_padding = int(input("Enter the Season Number Padding: "))
+        episode_padding = int(input("Enter the Episode Number Padding: "))
+        if not os.path.exists(correctRoot):
+            os.mkdir(correctRoot)
+        if webseries_num == 1:
+            generic_rename("Breaking Bad", "\s7..*dr", "\sse?", "e")
+        elif webseries_num == 2:
+            generic_rename("Game of Thrones", "\..*en", "\s\-\s", "x")
+        elif webseries_num == 3:
+            generic_rename("Lucifer", "\..*en", "\s\-\s", "x")
+    else:
+        print("fy")
 
 
 regex_renamer()
